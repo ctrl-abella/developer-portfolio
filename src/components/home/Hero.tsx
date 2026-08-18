e328af43dc38ce81aa99d10ab12e564ba409f603
Franz Christian Abella <abellafranzchristian@gmail.com>
2026-08-19 02:32:27 +0800

    feat(Hero): added the first initial design of the hero section

---
+++ src/components/home/Hero.tsx
@@ -0,0 +1,28 @@
+import { FiGithub, FiLinkedin, FiMail } from "react-icons/fi";
+import Image from "next/image";
+
+
+export default function Hero(){
+    return(
+        <div className="mt-6 grid grid-cols-1 items-center justify-items-center gap-8 md:grid-cols-2 md:justify-items-start">
+            <div className="h-64 w-64 overflow-hidden rounded-full flex items-center justify-center">
+                <Image 
+                src="/images/profile/profile_picture.jpeg" 
+                alt="profile"
+                width={256}
+                height={256}
+                className="h-full w-full object-cover"
+                loading="eager"/>
+            </div>
+            <div className="flex flex-col">
+                <h1 className="text-4xl font-bold tracking-tight text-foreground md:text-5xl lg:text-6xl">Franz Christian Abella</h1>
+                <p className="mt-6 text-justify"> I’m a full-stack developer who enjoys turning ideas into practical web and mobile applications. I’m currently exploring cloud services and AI to build smarter solutions. </p>
+                <p className="mt-6 text-justify"> I work across the stack, from user interfaces to APIs, databases, and backend logic, while continuously improving my skills and exploring new technologies. </p>
+                <div className="mt-6 flex flex-row gap-8 px-1">
+                    <a href="https://github.com/ctrl-abella" className="h-10 flex items-center justify-center gap-2">Github<FiGithub className="h-5 w-5"/></a>
+                    <a href="https://www.linkedin.com/in/franzabella" className="h-10 flex items-center justify-center gap-2">Linkedin<FiLinkedin className="h-5 w-5"/></a>
+                </div>
+            </div>
+        </div>
+    )   
+}
--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -1,7 +1,44 @@
 export default function Navbar(){
     return(
-        <div>
-            <p>Franz Christian Abella</p>
-        </div>
+        <nav className="w-full sticky top-0 z-50 border-b border-neutral-200 bg-background">
+            <div className="mx-auto flex h-16 w-full items-center justify-between px-6">
+                <a
+                href="/"
+                className="text-lg font-bold tracking-tight text-neutral-950 antialiased"
+                >
+                Franz
+                </a>
+
+                <div className="flex items-center gap-8">
+                <a
+                    href="#about"
+                    className="text-sm text-neutral-600 transition-colors hover:text-neutral-950"
+                >
+                    About
+                </a>
+
+                <a
+                    href="#skills"
+                    className="text-sm text-neutral-600 transition-colors hover:text-neutral-950"
+                >
+                    Skills
+                </a>
+
+                <a
+                    href="#projects"
+                    className="text-sm text-neutral-600 transition-colors hover:text-neutral-950"
+                >
+                    Projects
+                </a>
+
+                <a
+                    href="#contact"
+                    className="text-sm text-neutral-600 transition-colors hover:text-neutral-950"
+                >
+                    Contact
+                </a>
+                </div>
+            </div>
+        </nav>
     )
 }
